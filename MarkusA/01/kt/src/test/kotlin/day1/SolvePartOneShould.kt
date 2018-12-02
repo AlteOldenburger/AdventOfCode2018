53342cfb96aad0bb0f066034c625bc0fe65645b4
Markus Amshove <scm@amshove.org>
2018-12-02 12:40:38 +0100

    Loesung 01 Part 1+2 Kotlin

---
+++ MarkusA/01/kt/src/test/kotlin/day1/SolvePartOneShould.kt
@@ -0,0 +1,28 @@
+package day1
+
+import org.amshove.kluent.shouldEqual
+import org.junit.Test
+
+class SolvePartOneShould {
+    private fun solve(vararg frequencies: Long) = solvePartOne(frequencies.toList())
+
+    @Test
+    fun `sum up two frequencies`() {
+        solve(2L, 5L).shouldEqual(7L)
+    }
+
+    @Test
+    fun `sum up a negative frequency`() {
+        solve(2, -5).shouldEqual(-3L)
+    }
+
+    @Test
+    fun `sum up negative frequencies`() {
+        solve(-2, -5).shouldEqual(-7L)
+    }
+
+    @Test
+    fun `sum up multiple frequencies`() {
+        solve(-2, 7, 9, -98, -5).shouldEqual(-89L)
+    }
+}
