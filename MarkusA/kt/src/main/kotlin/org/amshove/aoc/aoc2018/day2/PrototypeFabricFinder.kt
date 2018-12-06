d4e42dfd94e705f41794b304fad69c57baa9f5b6
Markus Amshove <scm@amshove.org>
2018-12-06 21:58:28 +0100

    Versuch Nr 2

--- MarkusA/kt/src/main/kotlin/org/amshove/aoc/aoc2018/day2/PrototypeFabricFinder.kt
+++ MarkusA/kt/src/main/kotlin/org/amshove/aoc/aoc2018/day2/PrototypeFabricFinder.kt
@@ -4,6 +4,14 @@ private fun String.differentLetters(to: String) =
     this.mapIndexed { index, id -> this[index] == to[index] }
         .count { isSame -> !isSame }
 
+private fun String.withDifference(to: String) =
+    this.mapIndexed { index, letter -> if (this[index] != to[index]) index else null }
+        .filterNotNull()
+        .first()
+        .let { diffedIndex ->
+            this.substring(0..diffedIndex) + this.substring(diffedIndex + 1)
+        }
+
 fun asd(boxIds: Iterable<String>) =
     boxIds
         .map { testedBoxId ->
@@ -12,7 +20,8 @@ fun asd(boxIds: Iterable<String>) =
             }
         }
         .flatten()
-        .first()
+        .take(2)
+        .let { it[0].withDifference(it[1]) }
 
 fun main(args: Array<String>) {
     println(asd(INPUT))
